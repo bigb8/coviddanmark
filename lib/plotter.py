84f7ff7acabb57224c92d9bfd714948ee3a9493a
Morten Bo Svendsen <mortenbosvendsen@gmail.com>
2020-03-20 09:41:06 +0100

    data ref + visuals
    also removed graphs

--- lib/plotter.py
+++ lib/plotter.py
@@ -1,4 +1,4 @@
-import os, sys,datetime
+import os, sys,datetime,time
 
 import numpy as np
 # import scipy.special
@@ -6,10 +6,14 @@ from scipy.optimize import curve_fit
 
 
 
-
+#BOKEH - library for visual presentation of data
 from bokeh.layouts import gridplot
 from bokeh.plotting import figure, output_file, show,save
-from bokeh.models import SingleIntervalTicker, LinearAxis,Range1d
+from bokeh.models import SingleIntervalTicker, LinearAxis,Range1d,Title
+
+
+
+
 
 #Path to lib folder
 libpath = os.path.realpath(__file__).split("plotter.")[0] + os.sep
@@ -18,7 +22,7 @@ libpath = os.path.realpath(__file__).split("plotter.")[0] + os.sep
 datap = libpath.split("lib")[0]+ os.sep  + "data DK" + os.sep
 
 # import data
-global hosp,ita,resp,deaths,week,weekday,yearday
+global hosp,ita,resp,deaths,week,weekday,yearday,latest,dater
 
 hosp = []
 ita = []
@@ -28,6 +32,10 @@ deaths = []
 week = []
 weekday = []
 yearday = []
+
+dater = []
+
+latest = np.zeros([4,1])
 
 def sigmoid(x, L ,x0, k, b):
     y = L / (1 + np.exp(-k*(x-x0)))+b
@@ -41,7 +49,7 @@ numbers = {"ita":ita,"hosp": hosp,"resp":resp,"deaths":deaths}
 colorshex = {"ita":"#FF847c","hosp": "#FECEA8","resp":"#E84A5F","deaths":"#2A363B"}
 
 def getdata(fileofinterest,getweek):
-    global hosp,ita,resp,deaths,week,weekday,numbers,yearday
+    global hosp,ita,resp,deaths,week,weekday,numbers,yearday,latest,dater
 
     with open(datap + fileofinterest + ".txt",'r') as f:
         for i,l in enumerate(f.readlines()):
@@ -64,16 +72,25 @@ def getdata(fileofinterest,getweek):
 
                     # print(y,m,d,data[1],data[1][7:8])
                     mydate = datetime.datetime(y, m, d, 12, 00, 00)
+                    dater.append(str(d) + "." + str(m))
                     dayofyear = mydate.timetuple().tm_yday
                     yearday.append(dayofyear)
+
+        # Date and time for latest datapoint
+        latest[0] = int(data[-3][:4]) #Year
+        latest[1] = int(data[-3][4:6]) # Month
+        latest[2] = int(data[-3][6:8]) # Day
+        latest[3] = int(data[2]) # Time
 
 
 
 getdata("ita",False)
-getdata("hosp",True)
 getdata("resp",False)
 getdata("deaths",False)
+getdata("hosp",True) # Call hospitalizations at the end as data is more recent than deaths
 
+
+print(latest)
 
 #Conversion to numpy
 ita = np.array(ita)
@@ -85,24 +102,27 @@ yearday = np.array(yearday)
 
 maxy = np.max(hosp) + 25
 
-##
-tickdict = {70: '10.3', 77: '17.3', 84: '23.3', 91: '30.3'}
+## Creating x-axis.
+
+tickdict = {72:"12.3", 74:"14.3"} # Dict for displaying dates instead of days. Hardcoded some dates from beginning
+dayinterval = 1 # Interval between dates shown
+
+
+for i,dd in enumerate(dater):
+    print(i,dd)
+    tickdict[int(yearday[i])] = dater[i] #index in tickdect must be forced INT due to some JSON thing. https://github.com/bokeh/bokeh/issues/8166
+
+#Add XX days for x axis for regressions.
+for ik in range(1,4):
+    mydtt = datetime.timedelta(days=ik) + datetime.datetime(2020, int(dater[-1].split(".")[-1]) , int(dater[-1].split(".")[0]), 12, 00, 00)
+    tickdict[int(yearday[-1] +ik)] = str(mydtt.day) + "." +str(mydtt.month)
 
 
 
-##
-p = figure(title="Antal indlagte på intensiv - COVID19", tools='', background_fill_color="#fafafa")
-output_file('ita.html', title="Intensiv DK")
-p.quad(top=ita, bottom=0, left=yearday -.2, right=yearday+.2,fill_color=colorshex["ita"], line_color="white", alpha=1)
-
-p.xaxis.axis_label = 'Dato'
-p.yaxis.axis_label = 'Antal'
-p.xaxis.ticker =  SingleIntervalTicker(interval=.1, num_minor_ticks=0)
-p.y_range=Range1d(0, maxy)
-
-save(p)
 
 
+
+#Regressions
 xdata = (yearday - yearday[0]) + .1
 ydata = hosp
 
@@ -110,89 +130,83 @@ p0 = [max(ydata), np.median(xdata),1,min(ydata)] # this is an mandatory initial 
 popt, pcov = curve_fit(sigmoid, xdata, ydata,p0, method='dogbox')
 poptexp, pcovexp = curve_fit(expfunc, xdata, ydata,method='dogbox')
 
-# popt, pcov = curve_fit(sigmoid, weekday, hosp, method='dogbox')
-
 print(popt,pcov)
 
-fakex = np.linspace(0,np.max(xdata) - xdata[0] + 3,150) #week 1. 16.3.2020. Fremskriver seneste situation med 3 dage
+fakex = np.linspace(0,np.max(xdata) - xdata[0],150) #week 1. 16.3.2020.
+fakex_forward = np.linspace(np.max(xdata) - xdata[0],np.max(xdata) - xdata[0] + 3,50) #Fremskriver seneste situation med 3 dage
 
 y = sigmoid(fakex, *popt)
 yexp = expfunc(fakex,*poptexp)
 
-p1 = figure(title="Antal indlagte - COVID19", tools='', background_fill_color="#fafafa")
+y_forward = sigmoid(fakex_forward, *popt)
+yexp_forward = expfunc(fakex_forward,*poptexp)
+
+
+
+#Graph with regressions
+p1 = figure(title="Antal indlagte - COVID19 - Danmark", tools='', background_fill_color="#fafafa")
+p1.add_layout(Title(text="Data kilde: Statens Serum Institut", text_font_style="italic",text_font_size="8pt"), 'below')
+p1.add_layout(Title(text="Data fra kl:" + str(int(latest[-1][0])) + " " + str(int(latest[-2][0])) + "." + str(int(latest[-3][0])) + "." + str(int(latest[0][0])), text_font_style="italic",text_font_size="8pt"), 'above')
+p1.add_layout(Title(text="Visuel præsentation: coviddanmark.github.com - refenceliste på adressen", text_font_style="italic",text_font_size="8pt"), 'below')
 output_file('hosp.html', title="Indlagte DK")
-# p1.quad(top=hosp, bottom=0, left=weekday -.015, right=weekday+.015,fill_color=colorshex["hosp"], line_color="white", alpha=1)
 
 p1.quad(top=hosp, bottom=0, left=yearday -.2, right=yearday+.2,fill_color=colorshex["hosp"], line_color="white", alpha=1)
-
-
-p1.line(fakex+yearday[0],y,line_color="#59c25d", line_width=2, alpha=0.7, legend_label="Sigmoid fit")
-p1.line(fakex+yearday[0],yexp,line_color="#ff8888", line_width=4, alpha=0.7, legend_label="Exponentielt fit")
+p1.line(fakex+yearday[0],y,line_color="#59c25d", line_width=4, alpha=0.8, legend_label="Sigmoid fit")
+p1.line(fakex+yearday[0],yexp,line_color="#ff8888", line_width=4, alpha=0.8, legend_label="Exponentielt fit")
+p1.line(fakex_forward+yearday[0],y_forward,line_color="#59c25d", line_width=4, alpha=0.4,line_dash=[2,2], legend_label="Sigmoid fit")
+p1.line(fakex_forward+yearday[0],yexp_forward,line_color="#ff8888", line_width=4, alpha=0.4,line_dash=[2,2], legend_label="Exponentielt fit")
 
 
 p1.xaxis.axis_label = 'Dato'
 p1.yaxis.axis_label = 'Antal'
-p1.xaxis.ticker =  SingleIntervalTicker(interval=7, num_minor_ticks=7)
+p1.xaxis.ticker =  SingleIntervalTicker(interval=dayinterval, num_minor_ticks=dayinterval)
 p1.y_range=Range1d(0, np.max(y)+50)
 p1.legend.location = 'top_left'
 p1.xaxis.major_label_overrides =tickdict
 save(p1)
 
 
-p2 = figure(title="Antal i respirator - COVID19", tools='', background_fill_color="#fafafa")
-output_file('resp.html', title="i respirator DK")
-p2.quad(top=resp, bottom=0, left=yearday -.2, right=yearday+.2,fill_color=colorshex["resp"], line_color="white", alpha=1)
-
-p2.xaxis.axis_label = 'Dato'
-p2.yaxis.axis_label = 'Antal'
-p2.xaxis.ticker =  SingleIntervalTicker(interval=7, num_minor_ticks=7)
-p2.y_range=Range1d(0, maxy)
-p2.xaxis.major_label_overrides =tickdict
-
-save(p2)
 
 
-
-p3 = figure(title="Antal døde - COVID19", tools='', background_fill_color="#fafafa")
-output_file('deaths.html', title="Døde DK")
-p3.quad(top=deaths, bottom=0, left=yearday -.2, right=yearday+.2,fill_color=colorshex["deaths"], line_color="white", alpha=1)
-
-p3.xaxis.axis_label = 'Dato'
-p3.yaxis.axis_label = 'Antal'
-p3.xaxis.ticker =  SingleIntervalTicker(interval=7, num_minor_ticks=7)
-p3.y_range=Range1d(0, maxy)
-p3.xaxis.major_label_overrides =tickdict
-
-save(p3)
-
-p4 = figure(title="Oversigt indlagte - COVID19", tools='', background_fill_color="#fafafa")
+##Summary plot of hospitalized
+p4 = figure(title="Oversigt indlagte - COVID19 - Danmark", tools='', background_fill_color="#fafafa")
 output_file('stacked.html', title="Oversigt DK")
-p4.quad(top=hosp, bottom=0, left=yearday -.2, right=yearday+.1,fill_color=colorshex["hosp"], line_color="white", alpha=1,legend_label="Indlagt")
-p4.quad(top=ita, bottom=0, left=yearday -.15, right=yearday+.1,fill_color=colorshex["ita"], line_color="white", alpha=1,legend_label="på Intensiv")
-p4.quad(top=resp, bottom=0, left=yearday -.10, right=yearday+.1,fill_color=colorshex["resp"], line_color="white", alpha=1,legend_label="i Respirator")
-p4.quad(top=deaths, bottom=0, left=yearday +.10, right=yearday+.25,fill_color=colorshex["deaths"], line_color="white", alpha=1,legend_label="Død - akkumuleret")
+p4.quad(top=hosp, bottom=0, left=yearday -.4, right=yearday+.1,fill_color=colorshex["hosp"], line_color="white", alpha=1,legend_label="Indlagt")
+p4.quad(top=ita, bottom=0, left=yearday -.2, right=yearday+.1,fill_color=colorshex["ita"], line_color="white", alpha=1,legend_label="på intensiv")
+p4.quad(top=resp, bottom=0, left=yearday -.10, right=yearday+.1,fill_color=colorshex["resp"], line_color="white", alpha=1,legend_label="i respirator")
+p4.quad(top=deaths, bottom=0, left=yearday +.10, right=yearday+.3,fill_color=colorshex["deaths"], line_color="white", alpha=1,legend_label="Død - akkumuleret")
+
+
+p4.add_layout(Title(text="Data kilde: Statens Serum Institut", text_font_style="italic",text_font_size="8pt"), 'below')
+p4.add_layout(Title(text="Data fra kl:" + str(int(latest[-1][0])) + " " + str(int(latest[-2][0])) + "." + str(int(latest[-3][0])) + "." + str(int(latest[0][0])), text_font_style="italic",text_font_size="8pt"), 'above')
+p4.add_layout(Title(text="Visuel præsentation: coviddanmark.github.com - refenceliste på adressen", text_font_style="italic",text_font_size="8pt"), 'below')
+
 
 p4.xaxis.axis_label = 'Dato'
 p4.yaxis.axis_label = 'Antal'
-p4.xaxis.ticker =  SingleIntervalTicker(interval=7, num_minor_ticks=7)
+p4.xaxis.ticker =  SingleIntervalTicker(interval=dayinterval, num_minor_ticks=dayinterval)
 p4.y_range=Range1d(0, maxy)
 p4.legend.location = 'top_left'
 p4.xaxis.major_label_overrides =tickdict
-
 save(p4)
 
-p5 = figure(title="Oversigt procent indlagte - COVID19", tools='', background_fill_color="#fafafa")
+
+## Percentages on ITA
+p5 = figure(title="Procenter vedr. indlagte - COVID19 - Danmark", tools='', background_fill_color="#fafafa")
+p5.add_layout(Title(text="Data kilde: Statens Serum Institut, JAMA", text_font_style="italic",text_font_size="8pt"), 'below')
+p5.add_layout(Title(text="Data fra kl:" + str(int(latest[-1][0])) + " " + str(int(latest[-2][0])) + "." + str(int(latest[-3][0])) + "." + str(int(latest[0][0])), text_font_style="italic",text_font_size="8pt"), 'above')
+p5.add_layout(Title(text="Visuel præsentation: coviddanmark.github.com - refenceliste på adressen", text_font_style="italic",text_font_size="8pt"), 'below')
 output_file('percent.html', title="Intensiv i procent DK")
 
-# p4.quad(top=hosp, bottom=0, left=weekday -.015, right=weekday+.010,fill_color=colorshex["hosp"], line_color="white", alpha=1,legend_label="Indlagt")
-p5.quad(top=(ita/hosp)*100, bottom=0, left=yearday -.10, right=yearday,fill_color=colorshex["ita"], line_color="white", alpha=1,legend_label="% af indlagte på Intensiv")
-p5.quad(top=(resp/hosp)*100, bottom=0, left=yearday, right=yearday+.1,fill_color=colorshex["resp"], line_color="white", alpha=1,legend_label="% af indlagte i Respirator")
+p5.quad(top=(ita/hosp)*100, bottom=0, left=yearday -.35, right=yearday,fill_color=colorshex["ita"], line_color="white", alpha=1,legend_label="% af indlagte på intensiv")
+p5.line([yearday[0], yearday[-1]+1],[16,16],line_color="#ff8888", line_width=3, alpha=0.8, legend_label="% på intensiv, Italien, 7.3.2020")
+p5.quad(top=(resp/hosp)*100, bottom=0, left=yearday, right=yearday+.35,fill_color=colorshex["resp"], line_color="white", alpha=1,legend_label="% af indlagte i respirator")
+
 p5.xaxis.axis_label = 'Dato'
 p5.yaxis.axis_label = '%'
-p5.xaxis.ticker =  SingleIntervalTicker(interval=7, num_minor_ticks=7)
+p5.xaxis.ticker =  SingleIntervalTicker(interval=dayinterval, num_minor_ticks=dayinterval)
 p5.yaxis.ticker =  SingleIntervalTicker(interval=5, num_minor_ticks=5)
 p5.y_range=Range1d(0, 30)
 p5.legend.location = 'top_left'
 p5.xaxis.major_label_overrides =tickdict
-
 save(p5)
